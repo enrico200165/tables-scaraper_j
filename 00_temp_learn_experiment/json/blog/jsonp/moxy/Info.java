4543c505bfa59d9cb580bb0f3720e1dc1806af75
Viali Enrico (EXT NTT DATA Italia) <enrico200165@gmail.com>
2020-01-08 10:37:15 +0100

    still working on ficing the build

--- 00_temp_learn_experiment/json/blog/jsonp/moxy/Info.java
+++
@@ -1,7 +0,0 @@
-package blog.jsonp.moxy;
-
-public class Info {
-
-	String da = "http://www.javacodegeeks.com/2013/07/eclipselink-moxy-and-the-java-api-for-json-processing-object-model-apis.html";
-	
-}
